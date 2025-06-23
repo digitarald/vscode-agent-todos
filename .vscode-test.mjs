45cc6a37b2ed9a94bc6732a989e1363120740dc3
Harald Kirschner <hkirschner@microsoft.com>
2025-06-22 23:17:26 -0700

    Update CI workflows and configurations

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from '@vscode/test-cli';
+import { resolve } from 'path';
 
 export default defineConfig({
 	files: 'out/test/**/*.test.js',
+	workspaceFolder: resolve('./test-workspace'),
+	launchArgs: ['--disable-extensions']
 });
